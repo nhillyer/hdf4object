892c9d5120800b5abae38d3b1c32fd31ba9cab6a
Nelson Hillyer <nelson.hillyer@gmail.com>
2010-06-15 06:45:38 -0400

    tnh - Separated source code from header file, requiring the replacement of templated functions with non-templated equivalents.

--- src/test.cpp
+++ src/test.cpp
@@ -5,20 +5,26 @@ using namespace std;
 int main(int argc, char** argv)
 {
 	if (*(++argv) == NULL)
+	{
+		cerr << "ERROR!" << std::endl;
 		return 1;
+	}
 	hdf4object* test = new hdf4object(new string(*argv));
-
-	float** array = test->setToArray<float>(new string("Total_Attenuated_Backscatter_532"));
+	cerr << "FILE CREATED" << endl;
+	cerr << sizeof(float ) << endl;
+	float* array = (float*) test->setToArray(new string("Total_Attenuated_Backscatter_532"), sizeof(float));
+	cerr << "ARRAY CREATED" << endl;
 
 	int* dims = test->getSetDimensions(new string("Total_Attenuated_Backscatter_532"));
-	
+	cerr << "DIMS CREATED" << endl;
+	cerr << array << endl;	
 	for (int i = 0; i < dims[0]; i++) {
 		for (int j = 0; j < dims[1]; j++) {
-			cout << array[i][j] << '\t';
+			cout << array[i * dims[1] + j] << '\t';
 		}
 		cout << endl;
 	}
-	test->freeArray<float>(array);	
+	//test->freeArray(array, sizeof(float));	
 	delete test;
 
 	return 0;
